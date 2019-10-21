39428fdbe4a04279a046f279ba1ebffafa45337c
Keith Vassallo <keith@icemalta.com>
2019-10-21 15:12:39 +0200

    First Commit

---
+++ js/spellcontrol.js
@@ -0,0 +1,124 @@
+var dictionary = new Typo("mt_MT", false, false, { dictionaryPath: "js/typo/dictionaries" });
+var spellingErrors = [];
+var mt_check;
+var selectedText;
+
+$( document ).ready(function() {
+  checkSpelling();
+  bindListeners();
+});
+
+function bindListeners() {
+  mt_check = $('.mt_check').highlightWithinTextarea({
+    highlight: highlightWords
+  });
+  $('.mt_check').keyup(function(e) {
+      if(e.keyCode == 32 || e.keyCode == 8 || e.keyCode == 46) { // space/backspace/delete
+          checkSpelling();
+      }
+  });
+  $(".mt_check").on('paste', function() {
+    checkSpelling();
+  });
+
+  // All of the below is for the context menu
+  // Show the menu when a word in the textarea is right-clicked
+  $(".mt_check").bind("contextmenu", function(event) {
+    event.preventDefault();
+    selectedText = getSelectionText();
+    suggestions = getSuggestions(selectedText);
+
+    let menuEntries = "";
+    $.each(suggestions, function(i, suggested){
+      menuEntries += "<li data-action='" + suggested + "'>" + suggested + "</li>";
+    });
+    if (menuEntries == "") {
+      menuEntries = "<li data-action='no'>No suggestions</li>";
+    }
+    menuEntries += "<li data-action='search'>Search Google for ''" + selectedText + "''</li>";
+    $('.custom-menu').html(menuEntries);
+
+    // Show contextmenu
+    $(".custom-menu").finish().toggle(100).css({
+      top: (event.pageY - $('#mt_spell_cont').position().top) + "px",
+      left: (event.pageX - parseInt($('#mt_spell_cont').css('marginLeft'))) + "px"
+    });
+  });
+
+  // Hide the menu if anywhere else is clicked
+  $(document).bind("mousedown", function(e) {
+    if (!$(e.target).parents(".custom-menu").length > 0) {
+      $(".custom-menu").hide(100);
+    }
+  });
+
+  // If the menu element is clicked
+  $(document).on('click', 'ul.custom-menu li', function(){
+    switch ($(this).attr("data-action")) {
+      case "no":
+        break;
+      case "search":
+        window.open('https://www.google.com/search?q='+selectedText, '_blank');
+        break;
+      default:
+        let userText = $('.mt_check').val();
+        let replacementWord = $(this).attr("data-action");
+        let re = new RegExp("\\b" + selectedText + "\\b", "g");
+        userText = userText.replace(re, replacementWord);
+        $('.mt_check').val(userText);
+        $(mt_check).highlightWithinTextarea('update');
+        break;
+    }
+    $(".custom-menu").hide(100);
+  });
+}
+
+function checkSpelling() {
+  let userText = $('.mt_check').val();
+  spellingErrors = [];
+
+  // Remove hyphens
+  userText = userText.replace(/-/g, ' ');
+
+  $.each(userText.split(' '), function(i, word) {
+    word = word.replace(/[.,?!\s]/g, ''); // Remove punctuation
+    if (!dictionary.check(word)) {
+      spellingErrors.push(word);
+    }
+  });
+
+  $(mt_check).highlightWithinTextarea('update');
+}
+
+function highlightWords() {
+  if (spellingErrors.length == 0) return;
+
+  let highlightArr = [];
+  $.each(spellingErrors, function(i, word) {
+    let highlightObj = {};
+    highlightObj.highlight = word;
+    highlightObj.className = 'red';
+    highlightArr.push(highlightObj);
+  });
+  return highlightArr;
+}
+
+function getSuggestions(word) {
+  return dictionary.suggest(word);
+}
+
+function getSelectionText() {
+  var text = "";
+  var activeEl = document.activeElement;
+  var activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
+  if (
+    (activeElTagName == "textarea" || activeElTagName == "input") &&
+    /^(?:text|search|password|tel|url)$/i.test(activeEl.type) &&
+    (typeof activeEl.selectionStart == "number")
+  ) {
+    text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
+  } else if (window.getSelection) {
+    text = window.getSelection().toString();
+  }
+  return text;
+}
---
+++ js/typo/dictionaries/mt_MT/README.txt
@@ -0,0 +1,21 @@
+mt_MT Hunspell Dictionary
+Version 2019.10.17
+http://linux.org.mt
+
+This dictionary was created from the Maltese word list for aspell,
+originally created by the late Ramon Casha. It was repackaged to 
+Hunspell by Keith Vassallo. 
+
+The following dictionaries are available:
+
+  mt_MT (Maltese)
+ 
+Note that the affix file for this dictionary is only a placeholder.
+Hence, certain suffixes will not work. For example:
+
+inħobb -> recognised
+inħobbu -> not recognised
+
+An affix file may become available if the repackager figures out how
+to do so, or if some kind soul who know what they're doing contributes 
+it.
